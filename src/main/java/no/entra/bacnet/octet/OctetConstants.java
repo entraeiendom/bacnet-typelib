44328eb8b0ac03daaa23b989575fe69ef51fce5d
baardl <bard.lind@gmail.com>
2021-03-29 18:09:50 +0200

    Parse ObjectName

--- src/main/java/no/entra/bacnet/octet/OctetConstants.java
+++ src/main/java/no/entra/bacnet/octet/OctetConstants.java
@@ -2,4 +2,5 @@ package no.entra.bacnet.octet;
 
 public class OctetConstants {
     public final static Octet ENCODING_UCS_2 = Octet.fromHexString("04");
+    public final static Octet ENCODING_UTF_8 = Octet.fromHexString("00");
 }
--- src/test/java/no/entra/bacnet/utils/HexUtilsTest.java
+++ src/test/java/no/entra/bacnet/utils/HexUtilsTest.java
@@ -1,5 +1,7 @@
 package no.entra.bacnet.utils;
 
+import no.entra.bacnet.octet.Octet;
+import no.entra.bacnet.octet.OctetConstants;
 import org.junit.jupiter.api.Test;
 
 import static no.entra.bacnet.utils.HexUtils.intToHexString;
@@ -10,5 +12,21 @@ class HexUtilsTest {
     @Test
     void intToHexStringTest() {
         assertEquals("0018", intToHexString(24,4));
+    }
+
+    @Test
+    void extendedValueUtf8() {
+        Octet encoding = OctetConstants.ENCODING_UTF_8;
+        String hexString = "4657464355";
+        String value = HexUtils.parseExtendedValue(encoding, hexString);
+        assertEquals("FWFCU", value);
+    }
+
+    @Test
+    void extendedValueOldUSEncoding() {
+        Octet encoding = OctetConstants.ENCODING_UCS_2;
+        String objectNameHexString = "0053004f004b005000310036002d004e004100450034002f004600430042002e003400330034005f003100300031002d0031004f0055003000300031002e00520054003000300031";
+        String value = HexUtils.parseExtendedValue(encoding,objectNameHexString);
+        assertEquals("SOKP16-NAE4/FCB.434_101-1OU001.RT001", value);
     }
 }
