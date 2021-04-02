91f6e15b4c62e1d1338f29ee4d7552ff78eb5f6a
baardl <bard.lind@gmail.com>
2021-04-02 10:56:08 +0200

    Verify ConfigurationRequest

---
+++ src/test/java/no/entra/bacnet/ConfigurationRequestTest.java
@@ -0,0 +1,27 @@
+package no.entra.bacnet;
+
+import no.entra.bacnet.objects.ObjectId;
+import no.entra.bacnet.objects.ObjectType;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class ConfigurationRequestTest {
+
+
+    @Test
+    void getObjectIdentifierTest() {
+        ConfigurationRequest configurationRequest = new ConfigurationRequest();
+        Map<String, String> source = new HashMap<>();
+        source.put("objectId", "Device_8");
+        configurationRequest.setSource(source);
+        ObjectId objectId = configurationRequest.getObjectIdentifier();
+        assertNotNull(objectId);
+        assertEquals(ObjectType.Device, objectId.getObjectType());
+        assertEquals("8", objectId.getInstanceNumber());
+    }
+}
