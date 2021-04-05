7a8fa8eace23f28ee6d9fe72375bc468536bc4da
baardl <bard.lind@gmail.com>
2021-04-05 20:02:04 +0200

    ReadPropertiesMultiple's ReadAccessSpecification

---
+++ src/test/java/no/entra/bacnet/objects/ReadAccessSpecificationTest.java
@@ -0,0 +1,33 @@
+package no.entra.bacnet.objects;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static no.entra.bacnet.objects.PropertyIdentifier.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ReadAccessSpecificationTest {
+
+    @Test
+    void buildHexString() {
+        List<PropertyIdentifier> propertyIdentifiers = new ArrayList<>();
+        propertyIdentifiers.add(ObjectName);
+        propertyIdentifiers.add(Description);
+        propertyIdentifiers.add(Units);
+        propertyIdentifiers.add(PropertyIdentifier.PresentValue);
+        ObjectId objectId = new ObjectId(no.entra.bacnet.objects.ObjectType.Device, "8");
+        ReadAccessSpecification readAccessSpecification = new ReadAccessSpecification(objectId, propertyIdentifiers);
+        String expectedHexString = "0c020000081e094d091c097509551f";
+        assertEquals(expectedHexString, readAccessSpecification.buildHexString());
+    }
+
+    @Test
+    void buildHexStringDynamic() {
+        ObjectId objectId = new ObjectId(no.entra.bacnet.objects.ObjectType.Device, "8");
+        ReadAccessSpecification readAccessSpecification = new ReadAccessSpecification(objectId, ObjectName, Description, Units, PresentValue );
+        String expectedHexString = "0c020000081e094d091c097509551f";
+        assertEquals(expectedHexString, readAccessSpecification.buildHexString());
+    }
+}
