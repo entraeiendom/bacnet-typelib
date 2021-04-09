16a76b843e1ae1134c5c59d28fc14ab5850047c8
baardl <bard.lind@gmail.com>
2021-04-09 14:34:34 +0200

    #4 Verify 4 properties where two gives error

--- src/test/java/no/entra/bacnet/services/ReadObjectPropertiesResultParserTest.java
+++ src/test/java/no/entra/bacnet/services/ReadObjectPropertiesResultParserTest.java
@@ -61,6 +61,15 @@ With Error
       }
     },
     {
+      "property-identifier": "units",
+      "read-result": {
+        "propertyAccessError": {
+          "errorClass": "property",
+          "errorCode": "unknown-property"
+        }
+      }
+    },
+    {
       "property-identifier": "present-value",
       "read-result": {
         "propertyAccessError": {
@@ -98,7 +107,7 @@ HexString: 30460e0c020000081e294c39014ec4020000084f294c39024ec4008000004f294c390
 class ReadObjectPropertiesResultParserTest {
 
     @Test
-    void parse() throws BacnetParserException {
+    void parseSingleObjectMultipleProperties() throws BacnetParserException {
         String hexString = "0c008000001e294d4e7514005549315f5a6f6e6554656d70657261747572654f291c4e750f00416e616c6f672056616c756520304f29754e915f4f29554e4441b3332c4f1f";
         ParserResult<ReadObjectPropertiesResult> parserResult = ReadObjectPropertiesResultParser.parse(hexString);
         assertNotNull(parserResult);
@@ -114,6 +123,26 @@ class ReadObjectPropertiesResultParserTest {
         assertEquals("Analog Value 0", resultList.get(1).getReadResult().get(PropertyIdentifier.Description));
         assertEquals(MeasurementUnit.NoUnits, resultList.get(2).getReadResult().get(PropertyIdentifier.Units));
         assertEquals(Float.parseFloat("22.3999862670898"), resultList.get(3).getReadResult().get(PropertyIdentifier.PresentValue));
+    }
+
+    @Test
+    void parseSingleObjectMultiplePropertiesWithError() throws BacnetParserException {
+        String hexString = "0c020000081e294d4e75060046574643554f291c4e751800465720536572696573204261636e6574204465766963654f29755e910291205f29555e910291205f1f";
+        ParserResult<ReadObjectPropertiesResult> parserResult = ReadObjectPropertiesResultParser.parse(hexString);
+        assertNotNull(parserResult);
+        assertTrue(parserResult.isParsedOk());
+        ReadObjectPropertiesResult propertiesResult = parserResult.getParsedObject();
+        assertNotNull(propertiesResult);
+        ObjectId objectId = new ObjectId(ObjectType.Device, 8);
+        assertEquals(objectId, propertiesResult.getObjectId());
+        List<ReadPropertyResult> resultList = propertiesResult.getResults();
+        assertNotNull(resultList);
+        assertEquals(4, resultList.size());
+        assertEquals("FWFCU", resultList.get(0).getReadResult().get(PropertyIdentifier.ObjectName));
+        assertEquals("FW Series Bacnet Device", resultList.get(1).getReadResult().get(PropertyIdentifier.Description));
+        assertNotNull(resultList.get(2).getReadResult().get(PropertyIdentifier.XxError));
+        assertNotNull(resultList.get(3).getReadResult().get(PropertyIdentifier.XxError));
+//        assertEquals(Float.parseFloat("22.3999862670898"), resultList.get(3).getReadResult().get(PropertyIdentifier.PresentValue));
     }
 
     @Test
