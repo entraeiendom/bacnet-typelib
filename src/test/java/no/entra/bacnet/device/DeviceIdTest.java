f083f50249bc76a38878849de738aaf6e912a6de
baardl <bard.lind@gmail.com>
2021-04-13 19:54:39 +0200

    Ensure objects are equals, even with hash.

--- src/test/java/no/entra/bacnet/device/DeviceIdTest.java
+++ src/test/java/no/entra/bacnet/device/DeviceIdTest.java
@@ -2,6 +2,9 @@ package no.entra.bacnet.device;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -14,5 +17,10 @@ class DeviceIdTest {
         assertEquals(device1, device1too);
         assertTrue(device1.equals(device1too));
         assertEquals(device1.hashCode(), device1too.hashCode());
+        Map<DeviceId, String> map = new HashMap<>();
+        map.put(device1, "Device1");
+        map.put(device1too, "DeviceToo");
+        assertEquals(1, map.size());
+        assertEquals("DeviceToo", map.get(device1));
     }
 }
